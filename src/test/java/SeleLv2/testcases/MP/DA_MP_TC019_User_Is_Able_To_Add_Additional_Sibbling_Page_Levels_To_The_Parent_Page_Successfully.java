3c0aee4908644f7c1ab363c977e03aa4027242f7
trangle972501 <trangle972501@gmail.com>
2020-04-07 23:26:31 +0700

    update code

---
+++ src/test/java/SeleLv2/testcases/MP/DA_MP_TC019_User_Is_Able_To_Add_Additional_Sibbling_Page_Levels_To_The_Parent_Page_Successfully.java
@@ -0,0 +1,35 @@
+package SeleLv2.testcases.MP;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import SeleLv2.testcases.BaseTest;
+import pages.HomePage;
+import pages.LoginPage;
+import utils.Constants;
+import utils.Log;
+
+public class DA_MP_TC019_User_Is_Able_To_Add_Additional_Sibbling_Page_Levels_To_The_Parent_Page_Successfully extends BaseTest {
+
+	LoginPage loginPage = new LoginPage();
+	HomePage homePage = new HomePage();
+	
+	@Test
+	public void DA_MP_TC011() throws Exception {
+		
+		Log.info("Step #1: Navigate to Dashboard login page");
+		Log.info("Step #2: Login with valid account");
+		loginPage.loginDA(Constants.USERNAME, Constants.PASSWORD);
+		Assert.assertEquals(homePage.getUserName(), Constants.USERNAME);
+		
+		Log.info("Step #3: Go to Global Setting -> Add page");
+		homePage.selectItemsSetting("Add Page");
+		
+		Log.info("Step #4: Try to go to Global Setting -> Add page again");
+		homePage.selectItemsSetting("Add Page");
+		
+		Log.info("Observe the current page -> User cannot go to Global Setting -> Add page while \"New Page\" dialog appears.");
+		Assert.assertTrue(homePage.isDialogPageDisplayed("New Page"), "Dialog Add Page isn't displayed");
+
+	}
+}
