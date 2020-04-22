075a2a99e029d07ed43de02cdc7a4d12322b3047
Trang <hattrang0709@gmail.com>
2020-04-22 18:36:55 +0700

    updated add new panel page

--- src/main/java/pages/AddNewPanel.java
+++ src/main/java/pages/AddNewPanel.java
@@ -8,17 +8,40 @@ import controls.common.Link;
 
 public class AddNewPanel extends GeneralPage {
 	
-	private Link radioType = new Link("//tbody/tr/td/label[contains(text(),' %s')]");
+	private Link radioType          = new Link("//tbody/tr/td/label[contains(text(),'%s')]");
 	private ComboBox cbbDataProfile = new ComboBox("id=cbbProfile");
-	private TextBox txtDisplayName = new TextBox("id=txtDisplayName");
-	private TextBox txtChartTitle = new TextBox("id=txtChartTitle");
-	private CheckBox chkShowTitle = new CheckBox("id=chkShowTitle");
+	private TextBox txtDisplayName  = new TextBox("id=txtDisplayName");
+	private TextBox txtChartTitle   = new TextBox("id=txtChartTitle");
+	private CheckBox chkShowTitle   = new CheckBox("id=chkShowTitle");
+	private ComboBox cbbChartType   = new ComboBox("id=cbbChartType");
+	private Link radioStyle         = new Link("//input[@type='radio' and @value='%s']");
+	private ComboBox cbbCategory    = new ComboBox("id=cbbCategoryField");
+	private TextBox txtCaptionOfCategory = new TextBox("id=txtCategoryXAxis");
+	private ComboBox cbbSeries           = new ComboBox("id=cbbSeriesField");
+	private TextBox txtCaptionOfSeries   = new TextBox("id=txtValueYAxis");
+	private Link radioLegends            = new Link("//input[@value='%s']");
+	private CheckBox chkSeries           = new CheckBox("id=chkSeriesName");
+	private CheckBox chkCategories       = new CheckBox("id=chkCategoriesName");
+	private CheckBox chkValue            = new CheckBox("id=chkValue");
+	private CheckBox chkPercentage       = new CheckBox("id=chkPercentage");
 	
-	private TextBox txtPageName = new TextBox("id=name");
-	private Button btnOK = new Button("//input[@id='OK']");
-	private ComboBox cbbParentPage = new ComboBox("id=parent");
-	private ComboBox cbbNumberOfColumns = new ComboBox("id=columnnumber");
-	private ComboBox cbbDisplayAfter = new ComboBox("id=afterpage");
-	private CheckBox chkIsPublic = new CheckBox("id=ispublic");
+	public void selectType(String type) {
+		radioType.setDynamicValue(type);
+		radioType.waitForVisibility();
+		radioType.click();
+	}
+	
+	public void selectStyle(String style) {
+		radioStyle.setDynamicValue(style);
+		radioStyle.waitForVisibility();
+		radioStyle.click();
+	}
+	
+	public void selectLegendse(String legend) {
+		radioLegends.setDynamicValue(legend);
+		radioLegends.waitForVisibility();
+		radioLegends.click();
+	}
+	
 
 }
